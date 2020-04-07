88ba27fe31daa4a8758a91b4237d9a4faef409bd
Leonid-droid <lealro2011@gmail.com>
2020-04-07 11:57:14 +0300

    Условные операторы

---
+++ Basics/basic_005/script.js
@@ -0,0 +1,69 @@
+// if (2 * 4 == 78) {
+//     console.log("Yes!");
+// } else {
+//     console.log("No!");
+// }
+
+let num = 40;
+// if (num < 49) {
+//     console.log('Мало!')
+// } else if (num > 100) {
+//     console.log('Много!')
+// } else {
+//     console.log('Верно!')
+// }
+
+// (num == 50) ? console.log('Верно.'): console.log('Не верно!');
+
+console.log(num);
+
+// switch (num) {
+//     case num < 49:
+//         console.log('Мало!');
+//         break;
+//     case num > 100:
+//         console.log('Много!');
+//         break;
+//     case 50:
+//         console.log('Точно!');
+//         break;
+//     default:
+//         console.log('Что-то пошло не так.');
+//         break;
+// }
+
+switch (num) {
+    case 40:
+        console.log("Мало.");
+        break;
+    case 50:
+        console.log("Точно.");
+        break;
+    default:
+        console.log("Не так.");        
+}
+
+// let expr = 'Oranges';
+
+// switch (expr) {
+//     case "Oranges":
+//       console.log("Oranges are $0.59 a pound.");
+//       break;
+//     case "Apples":
+//       console.log("Apples are $0.32 a pound.");
+//       break;
+//     case "Bananas":
+//       console.log("Bananas are $0.48 a pound.");
+//       break;
+//     case "Cherries":
+//       console.log("Cherries are $3.00 a pound.");
+//       break;
+//     case "Mangoes":
+//     case "Papayas":
+//       console.log("Mangoes and papayas are $2.79 a pound.");
+//       break;
+//     default:
+//       console.log("Sorry, we are out of " + expr + ".");
+//   }
+  
+//   console.log("Is there anything else you'd like?");
