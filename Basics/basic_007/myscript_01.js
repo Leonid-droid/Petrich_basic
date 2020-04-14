0f8a0f15baa2a200a4e642528b2019d9e08e59dc
Leonid-droid <lealro2011@gmail.com>
2020-04-14 21:04:10 +0300

    Задание 04

---
+++ Basics/basic_007/myscript_01.js
@@ -0,0 +1,15 @@
+let world = prompt('Ввести слово из 6 букв');
+
+function inser(data){
+    if (data.length != 6) {
+        console.log('Не равно 6!!!');
+    } else {
+        return (data);
+    }
+}
+let enotherIns = inser(world);
+if(enotherIns){
+   console.log(enotherIns); 
+} else {
+    console.log('Не верное количество знаков!');    
+}
--- Basics/tasck_01/script.js
+++ Basics/tasck_01/script.js
@@ -1,34 +1,62 @@
 'use strict';
-let money = +prompt('Ваш бюджет на месяц?'),
-    time = prompt('Введите дату в формате YYYY-MM-DD'),
-    appData = {
-        budget: money,
-        timeData: time,
-        expenses: {},
-        optionalExpenses: {},
-        income: [],
-        savings: false
-    };
 
-for (let i = 0; i < 2; i++) {
-    let a = prompt('Статья обязательных расходов.', ''),
-        b = +prompt('Во сколько обойдётся?', '');
-    if ((typeof (a)) === 'string' && (typeof (a)) != null && typeof (b) != null && a != '' && b != '' && a.length < 15) {
-        console.log('done');
-        appData.expenses[a] = b;
-    } else {
-        i = i - 1;
+let money, time;
+
+function start() {
+    money = +prompt('Ваш бюджет на месяц?', '');
+    time = prompt('Введите дату в формате YYYY-MM-DD', '');
+    while (isNaN(money) || money == '' || money == null) {
+        money = +prompt('Ваш бюджет на месяц?', '');
     }
 }
+start();
 
-appData.moneyPerDay = (appData.budget / 30);
-if(appData.moneyPerDay < 1000){
-    alert('Ваш доход мал.');
-} else if (appData.moneyPerDay > 5000){
-    alert('У вас хороший доход!');
-} else if(appData.moneyPerDay >= 1000 && appData.moneyPerDay <= 5000) {
-    alert('У вас средний доход');
-} else {
-    alert('Произошла ошибка!!!')
-}     
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: true
+};
+
+function chooseExpensive() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt('Статья обязательных расходов.', ''),
+            b = +prompt('Во сколько обойдётся?', '');
+        if ((typeof (a)) === 'string' && (typeof (a)) != null && typeof (b) != null && a != '' && b != '' && a.length < 15) {
+            console.log('done');
+            appData.expenses[a] = b;
+        } else {
+            i = i - 1;
+        }
+    }
+}
+chooseExpensive();
+
+function detectDayBudget() {
+    appData.moneyPerDay = (appData.budget / 30).toFixed();
+    alert('Ваш ежедневный бюджет: ' + appData.moneyPerDay);
+    if (appData.moneyPerDay < 1000) {
+        alert('Ваш доход мал.');
+    } else if (appData.moneyPerDay > 5000) {
+        alert('У вас хороший доход!');
+    } else if (appData.moneyPerDay >= 1000 && appData.moneyPerDay <= 5000) {
+        alert('У вас средний доход');
+    } else {
+        alert('Произошла ошибка!!!')
+    }
+}
+detectDayBudget();
+
+function checkSaving() {
+    if(appData.savings == true){
+        let save = +prompt('Какова сумма накоплений?'),
+        percent = +prompt('Какой процент?');
+        appData.monthImcom = (save/100/12*percent).toFixed(2);
+        alert('Ваш доход в месяц с депозита: ' + appData.monthImcom);
+    }
+}
+checkSaving();
+
 console.log(appData);
