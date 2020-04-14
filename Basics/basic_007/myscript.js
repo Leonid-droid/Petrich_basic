0021017c028d3a5c844fea1b6450ea566427ff3f
Leonid-droid <lealro2011@gmail.com>
2020-04-14 10:05:42 +0300

    Задание 2

---
+++ Basics/basic_007/myscript.js
@@ -0,0 +1,28 @@
+// function inser() {
+//     let world = prompt('Ввести слово из 6 букв', '');
+//     return(world);    
+// }
+// inser();
+// console.log(world.length);
+// if ((inser.length) < 6) {
+//     console.log(inser());
+//     alert('Введено слово с МЕНЬШИМ количеством символов', '');
+//     inser();
+// }
+// if ((inser.length) > 6) {
+//     console.log(inser());
+//     alert('Введено слово с БОЛЬШИМ количеством символов', '');
+//     inser();
+// } else {
+//     console.log(inser());
+//     alert(inser());
+// }
+//console.log(inser());
+function inser(){
+let world = prompt('Ввести слово из 6 букв');
+let num = world.length;
+return (num);
+}
+let res = inser();
+console.log(res);
+
